06a549618ffe9782c082bc17ad257222fb2a126c
Aaron Imbrock <aimbrock@gmail.com>
2023-08-25 14:24:30 -0600

    Code functions the same but is now OO

--- src/diarycli.py
+++ src/diarycli.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 import argparse
@@ -6,13 +8,6 @@ from subprocess import call
 from datetime import date
 from datetime import datetime
 
-
-DIARY_DIR = os.environ.get("DIARY_DIR")
-DIARY_EDITOR = os.environ.get("DIARY_EDITOR")
-if DIARY_DIR is None:
-    DIARY_DIR = os.path.join(Path.home(), "diary")
-if DIARY_EDITOR is None:
-    DIARY_EDITOR = "vim"
 
 DIARY_TEMPLATE_ACCOUNTABILITY_PARTNER = """
 
@@ -74,57 +69,66 @@ DIARY_TEMPLATE = """
 """
 
 
-def parse_date(date_str):
-    try:
-        return datetime.strptime(date_str, "%Y-%m-%d").date()
-    except ValueError as e:
-        raise SystemExit(e)
-#
-#
-# def read_template(template_file):
-#     with open(template_file, "r") as template:
-#         fp = template.read()
-#         return fp
+class DiaryEntry:
+    def __init__(self, date, content):
+        self.date = date
+        self.content = content
 
+class Diary:
+    def __init__(self, diary_dir=None, diary_editor=None):
+        if diary_dir is None:
+            diary_dir = os.path.join(Path.home(), "diary")
+        if diary_editor is None:
+            diary_editor = "vim"
 
-def edit_entry(target_date = None):
-    if not target_date:
+        self.DIARY_DIR = diary_dir
+        self.DIARY_EDITOR = diary_editor
+
+    def parse_date(self, date_str):
+        try:
+            return datetime.strptime(date_str, "%Y-%m-%d").date()
+        except ValueError as e:
+            raise SystemExit(e)
+
+    def edit_entry(self, target_date=None):
+        if not target_date:
+            target_date = date.today()
+        entry_directory = os.path.join(self.DIARY_DIR, str(target_date.year), "{:02}".format(target_date.month))
+        if not os.path.isdir(entry_directory):
+            os.makedirs(entry_directory)
+        date_str = target_date.strftime("%Y-%m-%d")
+        file_name = date_str + ".md"
+        entry_path = os.path.join(os.path.join(entry_directory, file_name))
+        entry_exist = False
+        if os.path.isfile(entry_path):
+            entry_exist = True
+        with open(entry_path, "a") as entry:
+            template_file = DIARY_TEMPLATE
+            if not entry_exist:
+                entry.write(datetime.strftime(target_date, "## %Y-%m-%d, %A"))
+                entry.write(template_file)
+            entry.flush()
+            call([self.DIARY_EDITOR, entry_path])
+
+    def cat_entry(self):
         target_date = date.today()
-    entry_directory = os.path.join(DIARY_DIR, str(target_date.year), "{:02}".format(target_date.month))
-    if not os.path.isdir(entry_directory):
-        os.makedirs(entry_directory)
-    date_str = target_date.strftime("%Y-%m-%d")
-    file_name = date_str + ".md"
-    entry_path = os.path.join(os.path.join(entry_directory, file_name))
-    entry_exist = False
-    if os.path.isfile(entry_path):
-        entry_exist = True
-    with open(entry_path, "a") as entry:
-        template_file = DIARY_TEMPLATE
-        if not entry_exist:
-            entry.write(datetime.strftime(target_date, "## %Y-%m-%d, %A"))
-            entry.write(template_file)
-        entry.flush()
-        call([DIARY_EDITOR, entry_path])
-
-
-def cat_entry():
-    target_date = date.today()
-    entry_directory = os.path.join(DIARY_DIR, str(target_date.year), "{:02}".format(target_date.month))
-    file_name = target_date.strftime("%Y-%m-%d") + ".md"
-    entry_path = os.path.join(os.path.join(entry_directory, file_name))
-    if os.path.isfile(entry_path):
-        with open(entry_path, "r") as entry:
-            print(entry.read())
-    else:
-        print("Diary for today not created yet")
-
+        entry_directory = os.path.join(self.DIARY_DIR, str(target_date.year), "{:02}".format(target_date.month))
+        file_name = target_date.strftime("%Y-%m-%d") + ".md"
+        entry_path = os.path.join(os.path.join(entry_directory, file_name))
+        if os.path.isfile(entry_path):
+            with open(entry_path, "r") as entry:
+                print(entry.read())
+        else:
+            print("Diary for today not created yet")
 
 if __name__ == "__main__":
     args = sys.argv[1:]
+    diary = Diary()
+
     if not args:
-        edit_entry()
+        diary.edit_entry()
     elif args[0] == "cat":
-        cat_entry()
+        diary.cat_entry()
     else:
-       edit_entry(parse_date(args[0]))
+        target_date = diary.parse_date(args[0])
+        diary.edit_entry(target_date)
